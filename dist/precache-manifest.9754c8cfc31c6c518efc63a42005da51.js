99d0e4e8658a34ae6cb378713c2f8d7f02258669
Shivkumar <shivkumar@Shivkumars-Air.home>
2020-01-27 16:24:41 -0500

    installed service workers which can run webpage even in offline mode

---
+++ dist/precache-manifest.9754c8cfc31c6c518efc63a42005da51.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b34c9d480b870529c0e44060b5165b13",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f19d2f16352a75d72b2f",
+    "url": "main.css"
+  },
+  {
+    "revision": "f19d2f16352a75d72b2f",
+    "url": "main.js"
+  }
+]);
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,8 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 module.exports = {
    entry:'./src/client/index.js',
    mode: 'production',
@@ -28,6 +30,7 @@ plugins: [
         template: "./src/client/views/index.html",
         filename: "./index.html",
     }),
-    new MiniCssExtractPlugin({filename: '[name].css'})
+    new MiniCssExtractPlugin({filename: '[name].css'}),
+    new WorkboxPlugin.GenerateSW()
 ]
 }
